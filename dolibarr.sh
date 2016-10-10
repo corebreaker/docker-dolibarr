e8359a7a8a933d0224ae7550e9113c62677a81e1
corebreaker <frederic.meyer.77@gmail.com>
2016-10-10 13:52:19 +0300

    Correction du script pour changer le nom de la repository

--- dolibarr.sh
+++ dolibarr.sh
@@ -3,14 +3,20 @@ cd `dirname $0`
 
 case $1 in
 build)
-    docker build -t corebreaker.dolibarr/dolibarr:latest . ;;
+    docker build -t corebreaker/docker-dolibarr:latest . ;;
 
 create)
     docker volume create --name dolibarr-socket
     docker volume create --name dolibarr-db
     docker volume create --name dolibarr-data
-    docker run -d --name dolibarr.db -v dolibarr-socket:/var/run/mysqld:nocopy -v dolibarr-db:/var/lib/mysql -p 2222:22 corebreaker.dolibarr/dolibarr:latest db
-    docker run -d --name dolibarr.www -v dolibarr-socket:/var/run/mysqld:nocopy -v dolibarr-data:/var/lib/dolibarr -p 80 dolibarr:run
+    docker run -d --name dolibarr.db \
+        -v dolibarr-socket:/var/run/mysqld:nocopy \
+        -v dolibarr-db:/var/lib/mysql \
+        corebreaker/docker-dolibarr:latest db
+    docker run -d --name dolibarr.www \
+        -v dolibarr-socket:/var/run/mysqld:nocopy \
+        -v dolibarr-data:/var/lib/dolibarr \
+        -p 8080 corebreaker/docker-dolibarr:run
     ;;
 
 start)
@@ -18,9 +24,9 @@ start)
     docker start dolibarr.www
     ;;
 
-start)
-    docker start dolibarr.db
+stop)
     docker start dolibarr.www
+    docker start dolibarr.db
     ;;
 
 *)
